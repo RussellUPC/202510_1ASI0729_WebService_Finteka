ec574478c70779e777a9194791199d8ff9e6c927
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-06-16 09:28:05 -0500

    Add files via upload

---
+++ src/main/java/com/finteka/backend/FinTekaBackendApplication.java
@@ -0,0 +1,14 @@
+package com.finteka.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FinTekaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinTekaBackendApplication.class, args);
+    }
+
+}
