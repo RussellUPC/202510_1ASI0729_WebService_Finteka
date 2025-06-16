dc152b3c7002786a679468d0e9135c7ecdf21ca6
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-06-16 09:45:23 -0500

    Add files via upload

---
+++ src/test/java/com/finteka/backend/FinTekaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finteka.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTekaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
